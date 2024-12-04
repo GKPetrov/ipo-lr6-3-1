392ce9f1ea75ae5b99093e0448c07af8e99da1a7
Gleb <glebpetrof2007@gmail.com>
2024-12-04 15:40:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-a=["великий","Китай","партия","нефритовый","стержень","Xi"]
-str_count=0
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        if a[i][j]=='в':
-            str_count+=1
-            break
-print(str_count)
+str_list=["великий","Китай","партия","нефритовый","стержень","Xi"]
+strV_count=0
+for i in range(len(str_list)):
+    if 'в' in str_list[i]:
+        strV_count+=1
+        pass
+print(strV_count)
