f3739d76cd4ae06a072d84969c4104e1a2d586a1
Gleb <glebpetrof2007@gmail.com>
2024-12-12 10:39:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 str_list=["великий","Китай","партия","нефритовый","стержень","Xi"]
 strV_count=0
-for i in range(len(str_list)):
-    if 'в' in str_list[i]:
+for word in str_list:
+    if 'в' in word:
         strV_count+=1
         pass
 print(strV_count)
